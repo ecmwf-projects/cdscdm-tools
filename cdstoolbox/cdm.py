b25fa872b5ffb475143b5f0db846b4769755abf1
Alessandro Amici <a.amici@bopen.eu>
2020-04-15 17:59:07 +0200

    Check variable attribute and tests

--- cdstoolbox/cdm.py
+++ cdstoolbox/cdm.py
@@ -9,7 +9,7 @@ import xarray as xr
 LOGGER = structlog.get_logger()
 
 CDM_VARIABLES = {
-    "air_temperature": {"units": "K"},
+    "air_temperature": {"units": "K", "long_name": "temperature",},
 }
 
 
@@ -22,31 +22,31 @@ def check_coordinates(coords, log=LOGGER):
 
 
 def check_variable_attrs(attrs, log=LOGGER):
+    standard_name = attrs.get("standard_name")
+    units = attrs.get("units")
+
+    expected_attrs = CDM_VARIABLES.get(standard_name, {})
+    expected_units = expected_attrs.get("units")
+
+    if standard_name is not None:
+        log = log.bind(standard_name=standard_name)
+
     if "long_name" not in attrs:
-        log.warning("missing 'long_name' attribute for variable")
+        log.warning("missing recommended attribute 'long_name'")
 
     if "units" not in attrs:
-        log.warning("missing 'units' attribute for variable")
+        log.warning("missing recommended attribute 'units'")
     else:
-        units = attrs["units"]
         cf_units = cfunits.Units(units)
         if not cf_units.isvalid:
-            log.warning("'units' attribute not a valid unit", units=units)
-        expected_units = CDM_VARIABLES.get(attrs.get("standard_name"), {}).get("units")
-        if expected_units is not None:
+            log.warning("'units' attribute not valid", units=units)
+        elif expected_units is not None:
             expected_cf_units = cfunits.Units(expected_units)
+            log = log.bind(units=units, expected_units=expected_units)
             if not cf_units.equivalent(expected_cf_units):
-                log.warning(
-                    "'units' attribute not equivalent to the expected units",
-                    units=units,
-                    expected_units=expected_units,
-                )
+                log.warning("'units' attribute not equivalent to the expected")
             elif not cf_units.equals(expected_cf_units):
-                log.warning(
-                    "'units' attribute not equal to the expected units",
-                    units=units,
-                    expected_units=expected_units,
-                )
+                log.warning("'units' attribute not equal to the expected")
 
 
 def check_dataset(dataset, log=LOGGER):
--- tests/test_cdm.py
+++ tests/test_cdm.py
@@ -1,13 +1,56 @@
 import pathlib
 
 import pytest
+import structlog
 
 from cdstoolbox import cdm
+
+
+@pytest.fixture(name="log_output")
+def fixture_log_output():
+    return structlog.testing.LogCapture()
+
+
+@pytest.fixture(autouse=True)
+def fixture_configure_structlog(log_output):
+    structlog.configure(processors=[log_output])
 
 
 @pytest.fixture()
 def sampledir():
     return pathlib.Path(__file__).parent
+
+
+def test_check_variable_attrs(log_output):
+    cdm.check_variable_attrs({})
+    assert len(log_output.entries) == 2
+    assert "long_name" in log_output.entries[0]["event"]
+    assert "units" in log_output.entries[1]["event"]
+
+    cdm.check_variable_attrs({"standard_name": "*", "long_name": "*", "units": "*"})
+    assert len(log_output.entries) == 3
+    assert "units" in log_output.entries[2]["event"]
+
+    tas_attrs = {
+        "standard_name": "air_temperature",
+        "long_name": "air temperature",
+        "units": "K",
+    }
+    cdm.check_variable_attrs(tas_attrs)
+    assert len(log_output.entries) == 3
+
+    cdm.check_variable_attrs({**tas_attrs, "units": "m"})
+    assert len(log_output.entries) == 4
+    assert "units" in log_output.entries[3]["event"]
+
+    cdm.check_variable_attrs({**tas_attrs, "units": "°C"})
+    assert len(log_output.entries) == 5
+    assert "units" in log_output.entries[4]["event"]
+
+    cdm.check_variable_attrs({**tas_attrs, "standard_name": "*"})
+    assert len(log_output.entries) == 5
+
+    assert all(e["log_level"] == "warning" for e in log_output.entries)
 
 
 def test_open_netcdf_dataset(sampledir):
