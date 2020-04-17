3e5a63c8b653caef9708f92d571500749c0bde47
Alessandro Amici <a.amici@bopen.eu>
2020-04-17 15:37:21 +0200

    Sync data_vars to cds-cmor-tables

--- cdstoolbox/cdm.py
+++ cdstoolbox/cdm.py
@@ -32,14 +32,22 @@ def check_dataset_attrs(attrs, log=LOGGER):
             log.warning(f"missing recommended global attribute '{attr_name}'")
 
 
-def check_variable_attrs(attrs, log=LOGGER):
+def check_variable_attrs(name, attrs, log=LOGGER):
     standard_name = attrs.get("standard_name")
     units = attrs.get("units")
 
     log = log.bind(standard_name=standard_name)
 
-    expected_attrs = CDM_DATA_VARS.get(standard_name, {})
-    expected_units = expected_attrs.get("units")
+    definition = {}
+    if name in CDM_DATA_VARS:
+        definition = CDM_DATA_VARS[name]
+    elif standard_name is not None:
+        for expected_name, coord_def in CDM_DATA_VARS.items():
+            if coord_def.get("standard_name") == standard_name:
+                definition = coord_def
+                log.error("wrong name for coordinate", expected_name=expected_name)
+
+    expected_units = definition.get("units")
 
     if "long_name" not in attrs:
         log.warning("missing recommended attribute 'long_name'")
@@ -59,8 +67,8 @@ def check_variable_attrs(attrs, log=LOGGER):
                 log.warning("'units' attribute not equal to the expected")
 
 
-def check_coordinate_attrs(coord_name, attrs, dtype_name=None, log=LOGGER):
-    log = log.bind(coord_name=coord_name)
+def check_coordinate_attrs(name, attrs, dtype_name=None, log=LOGGER):
+    log = log.bind(coord_name=name)
 
     standard_name = attrs.get("standard_name")
     units = attrs.get("units")
@@ -68,8 +76,8 @@ def check_coordinate_attrs(coord_name, attrs, dtype_name=None, log=LOGGER):
     log = log.bind(standard_name=standard_name)
 
     definition = {}
-    if coord_name in CDM_COORDS:
-        definition = CDM_COORDS[coord_name]
+    if name in CDM_COORDS:
+        definition = CDM_COORDS[name]
     elif standard_name is not None:
         for expected_name, coord_def in CDM_COORDS.items():
             if coord_def.get("standard_name") == standard_name:
@@ -132,8 +140,9 @@ def open_netcdf_dataset(file_path):
     return xr.decode_cf(bare_dataset, use_cftime=False)
 
 
-def check_variable(data_var, log=LOGGER):
-    check_variable_attrs(data_var.attrs, log=log)
+def check_variable(data_var_name, data_var, log=LOGGER):
+    log.bind(data_var_name=data_var_name)
+    check_variable_attrs(data_var_name, data_var.attrs, log=log)
     check_variable_data(data_var, log=log)
 
 
@@ -143,7 +152,7 @@ def check_dataset(dataset, log=LOGGER):
         log.error("file must have at most one variable", data_vars=data_vars)
     check_dataset_attrs(dataset.attrs)
     for data_var_name, data_var in dataset.data_vars.items():
-        check_variable(data_var, log=log.bind(data_var_name=data_var_name))
+        check_variable(data_var_name, data_var, log=log)
     for coord_name, coord in dataset.coords.items():
         check_coordinate_attrs(coord_name, coord.attrs, coord.dtype.name, log=log)
 
@@ -167,10 +176,20 @@ def cmor_tables_to_cdm(cmor_tables_dir, cdm_path):
             cdm_coord["stored_direction"] = coord["stored_direction"]
         cdm_coords[coord["out_name"]] = cdm_coord
 
+    with open(cmor_tables_dir / "CDS_variable.json") as fp:
+        variable_entry = json.load(fp).get("variable_entry", {})
+
+    cdm_data_vars = {}
+    for coord in sorted(variable_entry.values(), key=lambda x: x["out_name"]):
+        cdm_data_var = {k: v for k, v in coord.items() if v and k in {"standard_name", "long_name"}}
+        if coord.get("units", "") and 'since' not in coord["units"]:
+            cdm_data_var["units"] = coord["units"]
+        cdm_data_vars[coord["out_name"]] = cdm_data_var
+
     cdm = {
         "attrs": ["title", "history", "institution", "source", "comment", "references"],
         "coords": cdm_coords,
-        "data_vars": CDM_DATA_VARS,
+        "data_vars": cdm_data_vars,
     }
     with open(cdm_path, "w") as fp:
         json.dump(cdm, fp, separators=(",", ":"), indent=1)
